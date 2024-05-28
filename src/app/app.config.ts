8256ee5eb82fd0a25a015ee4e551d5eccb46209a
maxjschuh <maximilianschuh@gmx.de>
2024-05-28 18:45:20 +0200

    fixed app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -10,5 +10,19 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({ "projectId": "simple-crm-71152", "appId": "1:975414178168:web:479eda1efd97d5fadad88d", "storageBucket": "simple-crm-71152.appspot.com", "apiKey": "AIzaSyA4EZ1BcZW_0HmPNkcbwR8xeWMZohkJf0U", "authDomain": "simple-crm-71152.firebaseapp.com", "messagingSenderId": "975414178168" }))), importProvidersFrom(provideFirestore(() => getFirestore())), provideCharts(withDefaultRegisterables()),]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp({ 
+      projectId: "simple-crm-71152", 
+      appId: "1:975414178168:web:479eda1efd97d5fadad88d", 
+      storageBucket: "simple-crm-71152.appspot.com", 
+      apiKey: "AIzaSyA4EZ1BcZW_0HmPNkcbwR8xeWMZohkJf0U", 
+      authDomain: "simple-crm-71152.firebaseapp.com", 
+      messagingSenderId: "975414178168" 
+    })),
+    provideFirestore(() => getFirestore()),
+    provideCharts(withDefaultRegisterables())
+  ]
 };
