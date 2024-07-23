7a565b3eea514af60ac105594d3955e95c919940
maxjschuh <maximilianschuh@gmx.de>
2024-07-23 09:00:15 +0200

    fixed: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,15 +10,14 @@ import { ImprintComponent } from './imprint/imprint.component';
 import { DataProtectionComponent } from './data-protection/data-protection.component';
 
 export const routes: Routes = [
-    { path: '**', component: DashboardComponent, title: "Easy-CRM | Dashboard"},
-    { path: "", component: DashboardComponent, title: "Easy-CRM | Dashboard"},
-    { path: "dashboard", component: DashboardComponent, title: "Easy-CRM | Dashboard"},
-    { path: "contacts", component: ContactsTableComponent, title: "Easy-CRM | Contacts"}, 
-    { path: "contacts/:id", component: ContactDetailComponent, title: "Easy-CRM | Contact-Details" },
-    { path: "transfers", component: TransfersTableComponent, title: "Easy-CRM | Transactions" },
-    { path: "transfers/:id", component: TransferDetailComponent, title: "Easy-CRM | Transaction-Details" },
-    { path: "employees", component: EmployeesTableComponent, title: "Easy-CRM | Employees" },
-    { path: "employees/:id", component: EmployeeDetailComponent, title: "Easy-CRM | Employee-Details" },
-    { path: "imprint", component: ImprintComponent, title: "Easy-CRM | Imprint"},
-    { path: "data-protection", component: DataProtectionComponent, title: "Easy-CRM | Data protection"}
+    { path: '', component: DashboardComponent, title: 'Easy-CRM | Dashboard'},
+    { path: 'dashboard', component: DashboardComponent, title: 'Easy-CRM | Dashboard'},
+    { path: 'contacts', component: ContactsTableComponent, title: 'Easy-CRM | Contacts'}, 
+    { path: 'contacts/:id', component: ContactDetailComponent, title: 'Easy-CRM | Contact-Details' },
+    { path: 'transfers', component: TransfersTableComponent, title: 'Easy-CRM | Transactions' },
+    { path: 'transfers/:id', component: TransferDetailComponent, title: 'Easy-CRM | Transaction-Details' },
+    { path: 'employees', component: EmployeesTableComponent, title: 'Easy-CRM | Employees' },
+    { path: 'employees/:id', component: EmployeeDetailComponent, title: 'Easy-CRM | Employee-Details' },
+    { path: 'imprint', component: ImprintComponent, title: 'Easy-CRM | Imprint'},
+    { path: 'data-protection', component: DataProtectionComponent, title: 'Easy-CRM | Data protection'}
 ];
